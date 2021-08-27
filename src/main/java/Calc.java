910503a53960811b62822c89ef8a9ce4f6a31238
ayushmkwn <ayushmkwn@gmail.com>
2021-08-27 17:24:30 +0530

    10. Multiple Delimiters of Any Length(GREEN)

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -7,42 +7,76 @@ public class Calc {
             return 0;
         }
         String[] nums;
-        StringBuilder delimiter;
+        String delimiter;
         String newIP=input;
         if(input.startsWith("//")) {
             if(isMultiDelimiterCheck(input)) {
-                delimiter = new StringBuilder(Character.toString(input.charAt(2)));
+                delimiter = Character.toString(input.charAt(2));
                 newIP = input.substring(4);
             } else {
                 List<Integer> lst = new ArrayList<>();
-                int len;
                 int begin = input.indexOf('[');
                 while (begin >= 0) {
                     lst.add(begin);
                     begin = input.indexOf('[',begin + 1);
                 }
 
-                if(lst.size() > 1) {
-                    delimiter = new StringBuilder("[");
-                    len = lst.size();
-
-                    for(int i = 0; i < len; i++) {
-                        int right = lst.get(i);
-                        delimiter.append(input.charAt(right + 1));
-                    }
-                    delimiter.append("]");
-                    int left = lst.get(len-1);
-                    newIP = input.substring(left + 4);
-                } else {
-                    delimiter = new StringBuilder(delimiterSplitter(input));
-                    newIP = substringGenerator(input);
+                List<Integer> lst2 = new ArrayList<>();
+                int end=input.indexOf(']');
+                while(end>=0){
+                    lst2.add(end);
+                    end=input.indexOf(']',end+1);
                 }
+                //int len;
+                int last = lst2.get(lst.size() - 1);
+
+                StringBuilder s = new StringBuilder();
+                int n = lst.size();
+                for(int i=0; i<n; i++) {
+                    int right = lst.get(i);
+                    int left = lst2.get(i);
+                    String temp = input.substring(right+1, left);
+                    if(temp.charAt(0) == '$' || temp.charAt(0) == '.' || temp.charAt(0) == '+' || temp.charAt(0) == '?' || temp.charAt(0) == '^' || temp.charAt(0) == '*') {
+                        int tempSize = temp.length();
+                        StringBuilder tempStr = new StringBuilder();
+                        for(int j=0; j<tempSize; j++) {
+                            tempStr.append("\\").append(temp.charAt(j));
+                        }
+                        s.append(tempStr);
+                    } else {
+                        s.append(temp);
+                    }
+
+                    if(i < n-1) {
+                        s.append("|");
+                    }
+                }
+                delimiter = s.toString();
+                newIP = input.substring(last + 2);
+
+//
+//                if(lst.size() > 1) {
+//                    delimiter = new StringBuilder("[");
+//                    len = lst.size();
+//
+//                    for(int i = 0; i < len; i++) {
+//                        int right = lst.get(i);
+//                        delimiter.append(input.charAt(right + 1));
+//                    }
+//                    delimiter.append("]");
+//                    int left = lst.get(len-1);
+//                    newIP = input.substring(left + 4);
+//                } else {
+//                    delimiter = new StringBuilder(delimiterSplitter(input));
+//                    newIP = substringGenerator(input);
+//                }
+
             }
         }
         else {
-            delimiter = new StringBuilder("[,\n]");
+            delimiter = ("[,\n]");
         }
-        nums=newIP.split(delimiter.toString());
+        nums=newIP.split(delimiter);
         return sum(nums);
     }
 
@@ -50,16 +84,16 @@ public class Calc {
         return input.charAt(2) != '[';
     }
 
-    private String delimiterSplitter(String input) {
-        int begin = input.indexOf('[');
-        int last = input.indexOf(']');
-        return input.substring(begin+1, last);
-    }
-
-    private String substringGenerator(String input) {
-        int last = input.indexOf(']');
-        return input.substring(last+2);
-    }
+//    private String delimiterSplitter(String input) {
+//        int begin = input.indexOf('[');
+//        int last = input.indexOf(']');
+//        return input.substring(begin+1, last);
+//    }
+//
+//    private String substringGenerator(String input) {
+//        int last = input.indexOf(']');
+//        return input.substring(last+2);
+//    }
 
     private int sum(String[] nums) {
         int sum;
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert;
+
+
+import static org.junit.Assert.assertEquals;
 
 public class CalcTest {
     Calc calc;
@@ -12,37 +14,37 @@ public class CalcTest {
 
     @Test
     public void isEmptyCheck() {
-        Assert.assertEquals(0,calc.add(""));
+        assertEquals(0,calc.add(""));
     }
 
     @Test
     public void isSingleValue() {
-        Assert.assertEquals(1,calc.add("1"));
+        assertEquals(1,calc.add("1"));
     }
 
     @Test
     public void sumofTwoValue() {
-        Assert.assertEquals(10,calc.add("5,5"));
+        assertEquals(10,calc.add("5,5"));
     }
 
     @Test
     public void sumofMultipleValue() {
-        Assert.assertEquals(15,calc.add("1,2,8,4"));
+        assertEquals(15,calc.add("1,2,8,4"));
     }
 
     @Test
     public void sumofMultipleValueNewLine() {
-        Assert.assertEquals(50,calc.add("20\n10\n19,1"));
+        assertEquals(50,calc.add("20\n10\n19,1"));
     }
 
     @Test
     public void allNewLine() {
-        Assert.assertEquals(0,calc.add("\n\n\n\n"));
+        assertEquals(0,calc.add("\n\n\n\n"));
     }
 
     @Test
     public void diffDelimiters() {
-        Assert.assertEquals(3,calc.add("//;\n1;2"));
+        assertEquals(3,calc.add("//;\n1;2"));
     }
 
     @Test
@@ -50,7 +52,7 @@ public class CalcTest {
         try {
             calc.add("5,-10,15,-20,30");
         } catch (IllegalArgumentException e) {
-            Assert.assertEquals("Negative numbers are not allowed: -10,-20",e.getMessage());
+            assertEquals("Negative numbers are not allowed: -10,-20",e.getMessage());
         }
     }
 
@@ -59,32 +61,32 @@ public class CalcTest {
         try {
             calc.add("-5");
         } catch (IllegalArgumentException e) {
-            Assert.assertEquals("Negative numbers are not allowed: -5",e.getMessage());
+            assertEquals("Negative numbers are not allowed: -5",e.getMessage());
         }
     }
 
     @Test
     public void greaterThanThousandIgnored() {
-        Assert.assertEquals(100,calc.add("10,50,1010,40"));
+        assertEquals(100,calc.add("10,50,1010,40"));
     }
 
     @Test
     public void greaterThanThousandIgnoredSingleValue() {
-        Assert.assertEquals(0,calc.add("1500"));
+        assertEquals(0,calc.add("1500"));
     }
 
     @Test
     public void anyLengthOfDelimiter() {
-        Assert.assertEquals(6,calc.add("//[xxx]\n1xxx2xxx3"));
+        assertEquals(6,calc.add("//[xxx]\n1xxx2xxx3"));
     }
 
     @Test
     public void multipleDelimiters() {
-        Assert.assertEquals(6,calc.add("//[;][,]\n1;2,3"));
+        assertEquals(6,calc.add("//[;][,]\n1;2,3"));
     }
 
     @Test
     public void multipleDelimitersofAnyLength() {
-        Assert.assertEquals(6,calc.add("//[;;][,]\n1;;;2,,3"));
+        assertEquals(6,calc.add("//[**][,][;]\n1**2,3"));
     }
 }
